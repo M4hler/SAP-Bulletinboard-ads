40bc82c28445adae6a53fa269e2a8e1b250aa28b
M4hler <oskar.makos.makowski@gmail.com>
2019-11-20 11:31:58 +0100

    Status after exercise 18.

--- src/main/java/com/sap/bulletinboard/ads/services/GetUserCommand.java
+++ src/main/java/com/sap/bulletinboard/ads/services/GetUserCommand.java
@@ -2,23 +2,26 @@ package com.sap.bulletinboard.ads.services;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.exception.HystrixBadRequestException;
 import com.sap.bulletinboard.ads.services.UserServiceClient.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 public class GetUserCommand extends HystrixCommand<User> {
-    // Hystrix uses a default timeout of 1000 ms, increase in case you run into problems in remote locations
-    private static final int DEFAULT_TIMEOUT_MS = 1000;
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     private String url;
     private RestTemplate restTemplate;
 
     public GetUserCommand(String url, RestTemplate restTemplate) {
-        super(HystrixCommandGroupKey.Factory.asKey("User"), DEFAULT_TIMEOUT_MS);
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("User"))
+                .andCommandKey(HystrixCommandKey.Factory.asKey("User.getById")));
         this.url = url;
         this.restTemplate = restTemplate;
     }
@@ -28,17 +31,41 @@ public class GetUserCommand extends HystrixCommand<User> {
         logger.info("sending request {}", url);
 
         try {
-            ResponseEntity<User> responseEntity = restTemplate.getForEntity(url, User.class);
+            ResponseEntity<User> responseEntity = sendRequest();
             logger.info("received response, status code: {}", responseEntity.getStatusCode());
             return responseEntity.getBody();
-        } catch(HttpStatusCodeException error) {
-            logger.error("received HTTP status code: {}", error.getStatusCode());
+        } catch(HttpServerErrorException error) {
+            logger.warn("received HTTP status code: {}", error.getStatusCode());
             throw error;
+        } catch(HttpClientErrorException error) {
+            logger.error("received HTTP status code: {}", error.getStatusCode());
+            throw new HystrixBadRequestException("Unsuccessful outgoing request", error);
         }
+    }
+
+    protected ResponseEntity<User> sendRequest() {
+        return restTemplate.getForEntity(url, User.class);
     }
 
     // this will be used in exercise 18
     protected int getTimeoutInMs() {
         return this.properties.executionTimeoutInMilliseconds().get();
+    }
+
+    @Override
+    protected User getFallback()
+    {
+        logger.info("enter fallback method");
+        if (isResponseTimedOut()) {
+            logger.error("execution timed out after {} ms (HystrixCommandKey:{})", getTimeoutInMs(),
+                    this.getCommandKey().name());
+        }
+        if (isFailedExecution()) {
+            logger.error("execution failed", getFailedExecutionException());
+        }
+        if (isResponseRejected()) {
+            logger.warn("request was rejected");
+        }
+        return new User();
     }
 }
